d1ef4ecdd446af5174af76145046a6dd012e2ae4
Luiz Gustavo Gonçalves Brito <112624030+luizbrito6@users.noreply.github.com>
2023-08-26 16:53:01 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,9 +1,11 @@
 import os 
 
+# Função para limpar o terminal 
 def x() : 
     os.system('cls')
 
 
+# Função que inputar um valor com o tipo primitivo específico   
 def leitorCoringa(tipoPrimitivo, msg, msgError) :
         
     while True : 
@@ -16,4 +18,14 @@ def leitorCoringa(tipoPrimitivo, msg, msgError) :
         else : 
             return x; 
 
+
+# Função para fazer um título de exercício 
+def titulo(titulo) :
+    print('-' * 40)
+    print(f'{titulo:^40}')
+    print('-' * 40)
+    print('\n')
+
+
+# Função para fazer um menu numerico 
 
